bff5701d2681234c49a4bd2752fcb6b0b34d94d1
ratchanon2318 <ratchanon.noknoy2318@gmail.com>
2026-01-23 11:58:59 +0700

    scripts for HOSxP data extraction

--- extract_patient_drug_allergy.sql
+++ extract_patient_drug_allergy.sql
@@ -1,9 +1,15 @@
-select o.*,   concat(p.pname,p.fname,' ',p.lname) as patient_name  ,ar.relation_name ,ag.allergy_group_name,
- asr.seiousness_name  ,ars.result_name,sp.name as spclty_name
- from opd_allergy o
- left outer join patient p on p.hn = o.hn
- left outer join allergy_group ag on ag.allergy_group_id = o.allergy_group_id
- left outer join allergy_seriousness asr on asr.seriousness_id = o.seriousness_id
- left outer join allergy_result ars on ars.allergy_result_id = o.allergy_result_id
- left outer join allergy_relation ar on ar.allergy_relation_id = o.allergy_relation_id
- left outer join spclty sp on sp.spclty = o.spclty
+SELECT
+    o.*,
+    CONCAT(p.pname, p.fname, ' ', p.lname) AS patient_name,
+    ar.relation_name,
+    ag.allergy_group_name,
+    asr.seriousness_name,
+    ars.result_name,
+    sp.name AS spclty_name
+FROM opd_allergy o
+LEFT OUTER JOIN patient p ON p.hn = o.hn
+LEFT OUTER JOIN allergy_group ag ON ag.allergy_group_id = o.allergy_group_id
+LEFT OUTER JOIN allergy_seriousness asr ON asr.seriousness_id = o.seriousness_id
+LEFT OUTER JOIN allergy_result ars ON ars.allergy_result_id = o.allergy_result_id
+LEFT OUTER JOIN allergy_relation ar ON ar.allergy_relation_id = o.allergy_relation_id
+LEFT OUTER JOIN spclty sp ON sp.spclty = o.spclty;
