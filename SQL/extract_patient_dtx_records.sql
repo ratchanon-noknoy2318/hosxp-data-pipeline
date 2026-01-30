088974df386395343eb2d98179cbd11a81597849
Ratchanon Noknoy <ratchanon.noknoy2318@gmail.com>
2026-01-30 17:31:26 +0700

    refactor sql to match HOSxP dtx screening logic

--- SQL/extract_patient_dtx_records.sql
+++ SQL/extract_patient_dtx_records.sql
@@ -9,13 +9,11 @@ SELECT
         WHEN 1 THEN 'ชาย'
         WHEN 2 THEN 'หญิง'
         ELSE 'ไม่ระบุ'
-    END AS sex_name
-FROM
-    patient p
-WHERE
-    (
-        p.sex = 1 AND p.pname IN ('นาง', 'นางสาว')
-        OR
-        p.sex = 2 AND p.pname = 'นาย'and   p.death = 'N'
-    )
-
+    END AS sex_name,
+    o.vstdate,
+    o.dtx
+FROM patient p
+JOIN opdscreen o ON o.hn = p.hn
+WHERE o.dtx IS NOT NULL
+  AND o.dtx <> ''
+  AND p.death = 'N';
