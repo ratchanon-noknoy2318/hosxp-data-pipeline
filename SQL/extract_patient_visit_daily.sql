b8247a3a2340be49078324fdde2f07e75bb49b02
Ratchanon Noknoy <ratchanon.noknoy2318@gmail.com>
2026-01-30 16:03:25 +0700

    update visit date condition to current date

--- SQL/extract_patient_visit_daily.sql
+++ SQL/extract_patient_visit_daily.sql
@@ -1,7 +1,11 @@
-SELECT ovst.hn,ovst.vsttime,ovst.spclty,
-concat(patient.fname,' ',patient.lname) as patient_name, patient.birthday , spclty.name
+SELECT ovst.hn,
+       ovst.vsttime,
+       ovst.spclty,
+       CONCAT(patient.fname,' ',patient.lname) AS patient_name,
+       patient.birthday,
+       spclty.name
 FROM ovst
-LEFT OUTER JOIN patient on patient.hn=ovst.hn
-LEFT OUTER JOIN spclty on spclty.spclty=ovst.spclty
-WHERE ovst.vstdate = '2025-06-20'
-ORDER BY spclty.name
+LEFT OUTER JOIN patient ON patient.hn = ovst.hn
+LEFT OUTER JOIN spclty ON spclty.spclty = ovst.spclty
+WHERE ovst.vstdate = CURDATE()
+ORDER BY spclty.name;
